a9dfecba3de03f8c03195aaac88cb6ef78d255bb
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2023-12-11 14:24:21 -0500

    Update App.cs
    Cleanup formatting

--- App.cs
+++ App.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (C) 2014 
 
 This program is free software: you can redistribute it and/or modify
@@ -23,113 +23,90 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 
-namespace RoboG
-{
-	public class App : ApplicationContext
-	{
-		private int _FormCount = 0;
+namespace RoboG {
+  public class App: ApplicationContext {
+    private int _FormCount = 0;
 
-		public App()
-		{
-			try
-			{
-				bool Start = false;
-				if (Processes.ProcessPathCount(Application.ExecutablePath) > 1)
-				{
-					System.Threading.Thread.Sleep(200);
+    public App() {
+      try {
+        bool Start = false;
+        if (Processes.ProcessPathCount(Application.ExecutablePath) > 1) {
+          System.Threading.Thread.Sleep(200);
 
-					if (Processes.ProcessPathCount(Application.ExecutablePath) > 1)
-					{
-						System.Threading.Thread.Sleep(2000);
+          if (Processes.ProcessPathCount(Application.ExecutablePath) > 1) {
+            System.Threading.Thread.Sleep(2000);
 
-						if (Processes.ProcessPathCount(Application.ExecutablePath) > 1)
-						{
-							Environment.Exit(0);
-						}
-					}
-				}
-				String[] Args = Environment.GetCommandLineArgs();
-				for (int i = 1; i < Args.Length; i++)
-				{
-					String[] SplittedArgs = Args[i].Split(':');
-					switch (SplittedArgs[0])
-					{
-						case "/s":
-							Start = true;
-							break;
-						case "/Admin":
-							GlobalData.Admin = true;
-							break;
-						case "/Settings":
-							if (SplittedArgs.Length > 1) Settings.CommonAppDataFile = SplittedArgs[1];
-							break;
-						case "/User":
-							if (SplittedArgs.Length > 1) Settings.Username = SplittedArgs[1];
-							break;
+            if (Processes.ProcessPathCount(Application.ExecutablePath) > 1) {
+              Environment.Exit(0);
+            }
+          }
+        }
+        String[] Args = Environment.GetCommandLineArgs();
+        for (int i = 1; i < Args.Length; i++) {
+          String[] SplittedArgs = Args[i].Split(':');
+          switch (SplittedArgs[0]) {
+          case "/s":
+            Start = true;
+            break;
+          case "/Admin":
+            GlobalData.Admin = true;
+            break;
+          case "/Settings":
+            if (SplittedArgs.Length > 1) Settings.CommonAppDataFile = SplittedArgs[1];
+            break;
+          case "/User":
+            if (SplittedArgs.Length > 1) Settings.Username = SplittedArgs[1];
+            break;
 
-						case "/Auto":
-							GlobalData.AutoExitCmd = true;
-							break;
-					}
-				}
+          case "/Auto":
+            GlobalData.AutoExitCmd = true;
+            break;
+          }
+        }
 
-				if (!Translations.LoadTranslations()) Environment.Exit(2);
+        if (!Translations.LoadTranslations()) Environment.Exit(2);
 
-				if (Start && Settings.LoadSettings(true, false))
-				{
-					if (!GlobalData.Admin && Alphaleonis.Win32.Vss.OperatingSystemInfo.OSVersionName > Alphaleonis.Win32.Vss.OSVersionName.WindowsServer2003 && !Processes.Elevated() && Settings.NeedsAdmin())
-					{
-						if (!Processes.RestartAsAdmin()) Environment.Exit(3);
-					}
-					else
-					{
-						frmMain frmHaupt = new frmMain();
-						frmHaupt.FormClosed += new FormClosedEventHandler(FormClosed);
-						frmHaupt.ShowForm += new BackupForm.ShowFormEventHandler(ShowForm);
-						frmHaupt.Show();
-						_FormCount++;
-					}
-				}
-				else
-				{
-					frmSettings frmSettings = new frmSettings();
-					frmSettings.FormClosed += new FormClosedEventHandler(FormClosed);
-					frmSettings.ShowForm += new BackupForm.ShowFormEventHandler(ShowForm);
-					frmSettings.Show();
-					_FormCount++;
-				}
-			}
-			catch (Exception ex)
-			{
-				HelperFunctions.ShowError(ex);
-				Environment.Exit(1);
-			}
-		}
+        if (Start && Settings.LoadSettings(true, false)) {
+          if (!GlobalData.Admin && Alphaleonis.Win32.Vss.OperatingSystemInfo.OSVersionName > Alphaleonis.Win32.Vss.OSVersionName.WindowsServer2003 && !Processes.Elevated() && Settings.NeedsAdmin()) {
+            if (!Processes.RestartAsAdmin()) Environment.Exit(3);
+          } else {
+            frmMain frmHaupt = new frmMain();
+            frmHaupt.FormClosed += new FormClosedEventHandler(FormClosed);
+            frmHaupt.ShowForm += new BackupForm.ShowFormEventHandler(ShowForm);
+            frmHaupt.Show();
+            _FormCount++;
+          }
+        } else {
+          frmSettings frmSettings = new frmSettings();
+          frmSettings.FormClosed += new FormClosedEventHandler(FormClosed);
+          frmSettings.ShowForm += new BackupForm.ShowFormEventHandler(ShowForm);
+          frmSettings.Show();
+          _FormCount++;
+        }
+      } catch (Exception ex) {
+        HelperFunctions.ShowError(ex);
+        Environment.Exit(1);
+      }
+    }
 
-		private void ShowForm(object Sender, BackupForm Form)
-		{
-			_FormCount++; // Must be before the close, as the program is otherwise terminated!
-			//((BackupForm)Sender).Close();
-			Form.Closed += new EventHandler(FormClosed);
-			Form.Show();
-		}
+    private void ShowForm(object Sender, BackupForm Form) {
+      _FormCount++; // Must be before the close, as the program is otherwise terminated!
+      //((BackupForm)Sender).Close();
+      Form.Closed += new EventHandler(FormClosed);
+      Form.Show();
+    }
 
-		void FormClosed(object Sender, EventArgs e)
-		{
-			try
-			{
-				_FormCount--;
-				if (_FormCount == 0)
-				{
-					Settings.CleanUp();
-					//ExitThread(); //Not always working
-					Environment.Exit(0);
-				}
-			}
-			catch (Exception Ex)
-			{
-				HelperFunctions.ShowError(Ex.Message);
-			}
-		}
-	}
+    void FormClosed(object Sender, EventArgs e) {
+      try {
+        _FormCount--;
+        if (_FormCount == 0) {
+          Settings.CleanUp();
+          //ExitThread(); //Not always working
+          Environment.Exit(0);
+        }
+      } catch (Exception Ex) {
+        HelperFunctions.ShowError(Ex.Message);
+      }
+    }
+  }
 }
