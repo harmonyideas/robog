36973a16fbc707954d46150ebc25a17a79a737d6
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2023-12-11 14:26:25 -0500

    Update File.cs
    Cleanup Formatting

--- File.cs
+++ File.cs
@@ -1,39 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace RoboG
-{
-	class File
-	{
-		public static void Delete(String Path)
-		{
-			String UNCPath = Directory.GetUNCPath(Path);
-			HelperFunctions.WriteLog(Translations.Get("DeletingFile") + Path);
-			if (!WinAPI.DeleteFile(UNCPath)) WinAPI.ThrowWin32Exception();
-		}
+namespace RoboG {
+  class File {
+    public static void Delete(String Path) {
+      String UNCPath = Directory.GetUNCPath(Path);
+      HelperFunctions.WriteLog(Translations.Get("DeletingFile") + Path);
+      if (!WinAPI.DeleteFile(UNCPath)) WinAPI.ThrowWin32Exception();
+    }
 
-		public static void Copy(string SourceFileName, string DestFileName)
-		{
-			Copy(SourceFileName, DestFileName, true);
-		}
+    public static void Copy(string SourceFileName, string DestFileName) {
+      Copy(SourceFileName, DestFileName, true);
+    }
 
-		public static void Copy(string SourceFileName, string DestFileName, bool Overwrite)
-		{
-			String UNCPath1 = Directory.GetUNCPath(SourceFileName);
-			String UNCPath2 = Directory.GetUNCPath(DestFileName);
-			if (!WinAPI.CopyFile(UNCPath1, UNCPath2, !Overwrite)) WinAPI.ThrowWin32Exception();
-		}
+    public static void Copy(string SourceFileName, string DestFileName, bool Overwrite) {
+      String UNCPath1 = Directory.GetUNCPath(SourceFileName);
+      String UNCPath2 = Directory.GetUNCPath(DestFileName);
+      if (!WinAPI.CopyFile(UNCPath1, UNCPath2, !Overwrite)) WinAPI.ThrowWin32Exception();
+    }
 
-		public static bool Exists(String Path)
-		{
-			String UNCPath = Directory.GetUNCPath(Path);
-			WinAPI.WIN32_FIND_DATA Data;
-			using (WinAPI.SafeFindHandle FindHandle = WinAPI.FindFirstFile(UNCPath, out Data))
-			{
-				if (FindHandle.IsInvalid) return false; else return true;
-			}
-		}
-	}
+    public static bool Exists(String Path) {
+      String UNCPath = Directory.GetUNCPath(Path);
+      WinAPI.WIN32_FIND_DATA Data;
+      using(WinAPI.SafeFindHandle FindHandle = WinAPI.FindFirstFile(UNCPath, out Data)) {
+        if (FindHandle.IsInvalid) return false;
+        else return true;
+      }
+    }
+  }
 }
