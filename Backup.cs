336475f5a05c60c2f3766811f17b868eccb3e67a
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2023-12-11 14:24:53 -0500

    Update Backup.cs

--- Backup.cs
+++ Backup.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (C) 2014 
 
 This program is free software: you can redistribute it and/or modify
@@ -21,148 +21,124 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
 
-namespace RoboG
-{
-	static class Backup
-	{
-		private static bool _Abort = false;
-		private static bool _MirOptionSet = false;
+namespace RoboG {
+  static class Backup {
+    private static bool _Abort = false;
+    private static bool _MirOptionSet = false;
 
-		public delegate void StartingBackupEventHandler();
-		public static event StartingBackupEventHandler StartingBackup;
+    public delegate void StartingBackupEventHandler();
+    public static event StartingBackupEventHandler StartingBackup;
 
-		public delegate void SizeDeterminedEventHandler(Int64 Size);
-		public static event SizeDeterminedEventHandler SizeDetermined;
+    public delegate void SizeDeterminedEventHandler(Int64 Size);
+    public static event SizeDeterminedEventHandler SizeDetermined;
 
-		private static List<String> _BackupDirectories = new List<String>();
-		private static List<String> _TargetDirectories = new List<String>();
+    private static List < String > _BackupDirectories = new List < String > ();
+    private static List < String > _TargetDirectories = new List < String > ();
 
-		public static void Initialize()
-		{
-			IO.Initialize();
-			_TargetDirectories = IO.GetBackupRoot(true);
-		}
+    public static void Initialize() {
+      IO.Initialize();
+      _TargetDirectories = IO.GetBackupRoot(true);
+    }
 
-		public static bool DoBackup()
-		{
-			try
-			{
-				_Abort = false;
-				_MirOptionSet = false;
+    public static bool DoBackup() {
+      try {
+        _Abort = false;
+        _MirOptionSet = false;
 
-				for (int i = 0; i < Settings.Sources.Count; i++)
-				{
-					Settings.Sources[i].NoChanges = false;
+        for (int i = 0; i < Settings.Sources.Count; i++) {
+          Settings.Sources[i].NoChanges = false;
 
-					if (Settings.Sources[i].MIR)
-					{
-						_MirOptionSet = true;
-					}
-				}
-				/* if (_MirOptionSet)
-				{
-					if (HelperFunctions.ShowYesNoQuestion("RoboG", Translations.Get("WarningMIR"), 2) != System.Windows.Forms.DialogResult.Yes)
-					{
-						_Abort = true;
-					}
-				} */
+          if (Settings.Sources[i].MIR) {
+            _MirOptionSet = true;
+          }
+        }
+        /* if (_MirOptionSet)
+        {
+        	if (HelperFunctions.ShowYesNoQuestion("RoboG", Translations.Get("WarningMIR"), 2) != System.Windows.Forms.DialogResult.Yes)
+        	{
+        		_Abort = true;
+        	}
+        } */
 
-				for (int i = 0; i < _TargetDirectories.Count; i++)
-				{
+        for (int i = 0; i < _TargetDirectories.Count; i++) {
 
-					if (_Abort) return false;
+          if (_Abort) return false;
 
-					if (_TargetDirectories[i] == null) return false;
-					if (!Directory.IsWriteable(_TargetDirectories[i]) && !System.IO.Directory.Exists(_TargetDirectories[i] + Settings.Subfolder))
-					{
-						HelperFunctions.ShowError(Translations.Get("DirectoryNotWriteable1") + _TargetDirectories[i] + Translations.Get("DirectoryNotWriteable2"));
-						return false;
-					}
-					if (Settings.Subfolder != "" && !System.IO.Directory.Exists(_TargetDirectories[i] + Settings.Subfolder)) Directory.Create(_TargetDirectories[i] + Settings.Subfolder);
-					if (!Directory.IsWriteable(_TargetDirectories[i] + Settings.Subfolder))
-					{
-						HelperFunctions.ShowError(Translations.Get("DirectoryNotWriteable1") + _TargetDirectories[i] + Settings.Subfolder + Translations.Get("DirectoryNotWriteable2"));
-						return false;
-					}
-					if (!Directory.TestAttributes(_TargetDirectories[i] + Settings.Subfolder))
-					{
-						HelperFunctions.ShowError(Translations.Get("DirectoryNoAttributes1") + _TargetDirectories[i] + Settings.Subfolder + Translations.Get("DirectoryNoAttributes2"));
-						return false;
-					}
-				}
+          if (_TargetDirectories[i] == null) return false;
+          if (!Directory.IsWriteable(_TargetDirectories[i]) && !System.IO.Directory.Exists(_TargetDirectories[i] + Settings.Subfolder)) {
+            HelperFunctions.ShowError(Translations.Get("DirectoryNotWriteable1") + _TargetDirectories[i] + Translations.Get("DirectoryNotWriteable2"));
+            return false;
+          }
+          if (Settings.Subfolder != "" && !System.IO.Directory.Exists(_TargetDirectories[i] + Settings.Subfolder)) Directory.Create(_TargetDirectories[i] + Settings.Subfolder);
+          if (!Directory.IsWriteable(_TargetDirectories[i] + Settings.Subfolder)) {
+            HelperFunctions.ShowError(Translations.Get("DirectoryNotWriteable1") + _TargetDirectories[i] + Settings.Subfolder + Translations.Get("DirectoryNotWriteable2"));
+            return false;
+          }
+          if (!Directory.TestAttributes(_TargetDirectories[i] + Settings.Subfolder)) {
+            HelperFunctions.ShowError(Translations.Get("DirectoryNoAttributes1") + _TargetDirectories[i] + Settings.Subfolder + Translations.Get("DirectoryNoAttributes2"));
+            return false;
+          }
+        }
 
-				if (_Abort) return false;
-				HelperFunctions.UpdateStatus(Translations.Get("LookingForChanges"));
-				HelperFunctions.WriteLog(Translations.Get("LookingForChanges"));
+        if (_Abort) return false;
+        HelperFunctions.UpdateStatus(Translations.Get("LookingForChanges"));
+        HelperFunctions.WriteLog(Translations.Get("LookingForChanges"));
 
-				if (_BackupDirectories.Count > 0) GlobalData.NoPreviousBackup = false;
+        if (_BackupDirectories.Count > 0) GlobalData.NoPreviousBackup = false;
 
-				Int64 SpaceNeeded = 0;
-				bool NoChanges = true;
+        Int64 SpaceNeeded = 0;
+        bool NoChanges = true;
 
-				if (Settings.Scan)
-				{
+        if (Settings.Scan) {
 
-					for (int i = 0; i < Settings.Sources.Count; i++)
-					{
-						long Space = IO.GetSpaceNeeded(Settings.Sources[i]);
-						if (Space == -1) return false;
-						SpaceNeeded += Space;
-						if (!Settings.Sources[i].NoChanges) NoChanges = false;
-					}
+          for (int i = 0; i < Settings.Sources.Count; i++) {
+            long Space = IO.GetSpaceNeeded(Settings.Sources[i]);
+            if (Space == -1) return false;
+            SpaceNeeded += Space;
+            if (!Settings.Sources[i].NoChanges) NoChanges = false;
+          }
 
+          if (_Abort) return false;
 
+          if (SizeDetermined != null) SizeDetermined(SpaceNeeded);
 
-					if (_Abort) return false;
+          if (NoChanges) {
+            HelperFunctions.WriteLog(Translations.Get("NoChanges"));
+            return true;
+          }
+        }
+        if (_Abort) return false;
+        if (StartingBackup != null) StartingBackup();
 
-					if (SizeDetermined != null) SizeDetermined(SpaceNeeded);
+        HelperFunctions.UpdateStatus(Translations.Get("BackingUp") + HelperFunctions.SizeSuffix(SpaceNeeded) + "...");
 
-					if (NoChanges)
-					{
-						HelperFunctions.WriteLog(Translations.Get("NoChanges"));
-						return true;
-					}
-				}
-				if (_Abort) return false;
-				if (StartingBackup != null) StartingBackup();
+        for (int i = 0; i < Settings.Sources.Count; i++) {
+          if (_Abort) return false;
+          HelperFunctions.WriteLog(Translations.Get("BackingUpSource") + (i + 1) + "...");
 
-				HelperFunctions.UpdateStatus(Translations.Get("BackingUp") + HelperFunctions.SizeSuffix(SpaceNeeded) + "...");
+          if (Settings.Sources[i].NoChanges) {
+            HelperFunctions.WriteLog(Translations.Get("NoChanges"));
+            continue;
+          }
 
+          IO.MirrorDirectory(Settings.Sources[i]);
+        }
 
-				for (int i = 0; i < Settings.Sources.Count; i++)
-				{
-					if (_Abort) return false;
-                    HelperFunctions.WriteLog(Translations.Get("BackingUpSource") + (i + 1) + "...");
-					
-                    if (Settings.Sources[i].NoChanges)
-					{
-						HelperFunctions.WriteLog(Translations.Get("NoChanges"));
-						continue;
-					}
+        HelperFunctions.WriteLog(Translations.Get("BackupCompleted"));
+        return true;
+      } catch (IOException ex) {
+        HelperFunctions.ShowError(ex);
+      } catch (Exception ex) {
+        HelperFunctions.ShowError(ex);
+      }
+      return false;
+    }
 
-					IO.MirrorDirectory(Settings.Sources[i]);
-				}
+    public static bool Abort() {
+      _Abort = true;
+      IO.Abort();
+      return true;
 
-				HelperFunctions.WriteLog(Translations.Get("BackupCompleted"));
-				return true;
-			}
-			catch (IOException ex)
-			{
-				HelperFunctions.ShowError(ex);
-			}
-			catch (Exception ex)
-			{
-				HelperFunctions.ShowError(ex);
-			}
-			return false;
-		}
-
-		public static bool Abort()
-		{
-			_Abort = true;
-			IO.Abort();
-			return true;
-
-		}
-	}
+    }
+  }
 }
